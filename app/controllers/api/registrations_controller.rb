8f8cd3c1745704a764a732b5d8e42eca65eba5a2
Skylar King <sking@crazyawesome.io>
2016-03-23 12:57:42 -0500

    Fixed API token using Devise auth vs. Doorkeeper. Update registrations controller to update either email or username depending on field provided in API. Update JSON to show username in API reg.

--- app/controllers/api/registrations_controller.rb
+++ app/controllers/api/registrations_controller.rb
@@ -3,15 +3,17 @@ class Api::RegistrationsController < ApplicationController
   protect_from_forgery with: :null_session
 
   def create
-    @user = User.new params.require(:user).permit(:email, :password)
+    @user = User.new params.require(:user).permit(:username, :password)
+    @user.username = @user.email if @user.email.present?
+    @user.email = "#{@user.username}@domain.com" if @user.username.present?
 
-    # TODO: Chnge permit by email to username and set email to username. this
-    # is for assignment completion only.
-    
+    # TODO: Change back to email after project submission.
+
     if @user.save
       render :user
     else
       render json: @user.errors, status: 422
     end
   end
+
 end

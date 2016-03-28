6d0b5eb9313cea02525af755cebcb35cc0afd661
Skylar King <sking@crazyawesome.io>
2016-03-28 12:50:40 -0500

    Add follower. Add styling for comments and to feed. Add comment and user relaitonship.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,46 +8,53 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by id: params[:id]
-    @followers = @user.followers
-
     follower_ids = @user.following_users.pluck(:id)
     all_ids = follower_ids << @user.id
     @posts = Post.where(user_id: all_ids).order("created_at DESC")
-
-    # TODO: Show user info
-    # TODO: This may end up being a duplicate for the feed now that I think about it.
-
   end
 
   def feed
     @user = User.find_by id: params[:id]
     @followers = @user.followers
-    # TODO: Get users posts
-    # TODO: Get posts from the users they follow
-    # TODO: Prevent viewers from seeing the posts from the users they follow.
   end
 
   def follow
     @user = User.find(params[:id])
-    current_user.follow(@user)
-    redirect_to :back
+    if @user != current_user
+      current_user.follow(@user)
+      redirect_to :back, :notice => "You are now following #{@user.email}."
+    else
+      redirect_to :back, :alert => "You cannot follow yourself!"
+    end
   end
 
   def unfollow
     @user = User.find(params[:id])
-    current_user.stop_following(@user)
-    redirect_to :back
+    if @user != current_user
+      current_user.stop_following(@user)
+      redirect_to :back, :notice => "You are not following #{@user.email}."
+    else
+      redirect_to :back, :alert => "You cannot unfollow yourself!"
+    end
   end
 
   def block
     @user = User.find(params[:id])
-    current_user.block(@user)
-    redirect_to :back
+    if @user != current_user
+      current_user.block(@user)
+      redirect_to :back, :notice => "You have blocked #{@user.email}."
+    else
+      redirect_to :back, :alert => "You cannot block yourself!"
+    end
   end
 
   def unblock
     @user = User.find(params[:id])
-    current_user.unblock(@user)
-    redirect_to :back
+    if @user != current_user
+      current_user.unblock(@user)
+      redirect_to :back, :notice => "You have unblocked #{@user.email}."
+    else
+      redirect_to :back, :alert => "You cannot unblock yourself!"
+    end
   end
 end
